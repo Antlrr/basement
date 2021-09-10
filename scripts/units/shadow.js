3e63a936d589f8b6a533bc57b883866a28662e87
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-09-10 22:59:24 +0800

    Aaaa

--- scripts/units/shadow.js
+++ scripts/units/shadow.js
@@ -2,15 +2,7 @@ const shadow = extend(UnitType, "shadow", {
 	draw(unit) {
 		this.super$draw(unit);
 		Draw.rect("shadow-spinner", unit.x, unit.y, Time.time * 12);
-                Draw.rect("shadow-spinner", unit.x, unit.y, (Time.time +6) * 12);
-                Draw.rect("shadow-spinner", unit.x, unit.y, (Time.time -6) * 12);
-                Draw.rect("shadow-spinner", unit.x, unit.y, (Time.time +12) * 12);
-                Draw.rect("shadow-spinner", unit.x, unit.y, (Time.time -12) * 12);
                 Draw.rect("shadow-spinner-outline", unit.x, unit.y, Time.time * 12);
-                Draw.rect("shadow-spinner-outline", unit.x, unit.y, (Time.time +6) * 12);
-                Draw.rect("shadow-spinner-outline", unit.x, unit.y, (Time.time -6) * 12);
-                Draw.rect("shadow-spinner-outline", unit.x, unit.y, (Time.time +12) * 12);
-                Draw.rect("shadow-spinner-outline", unit.x, unit.y, (Time.time -12) * 12);
                 }
              });
 shadow.constructor = () => extend(UnitEntity, {});
