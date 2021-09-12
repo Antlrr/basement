ea5d723782fa678c6eb9ace016321b6cf9098317
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-09-12 14:31:27 +0800

    Create purpleUnits.js

---
+++ scripts/units/purpleUnits.js
@@ -0,0 +1,21 @@
+//this is just so I can arrange the tiers in game
+
+const detonate = extendContent(UnitType, "detonate", {});
+detonate.constructor = () => extend(UnitEntity, {
+});
+
+const discharge = extendContent(UnitType, "discharge", {});
+discharge.constructor = () => extend(UnitEntity, {
+});
+
+const disentigrate = extendContent(UnitType, "disentigrate", {});
+disentigrate.constructor = () => extend(UnitEntity, {
+});
+
+const forcer = extendContent(UnitType, "forcer", {});
+forcer.constructor = () => extend(MechUnit, {
+});
+
+const impulse = extendContent(UnitType, "impulse", {});
+impulse.constructor = () => extend(LegsUnit, {
+});
