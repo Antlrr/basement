e70fa341dd1c118bf20e8642cd0b205708cbfd61
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-09-17 22:10:26 +0800

    Update other.js

--- scripts/units/other.js
+++ scripts/units/other.js
@@ -1,13 +1,3 @@
-const shadow = extend(UnitType, "shadow", {
-	draw(unit) {
-		this.super$draw(unit);
-		Draw.rect("basement-shadow-spinner", unit.x, unit.y, Time.time * 10);
-                Draw.rect("basement-shadow-spinner-outline", unit.x, unit.y, Time.time * 10);
-                Draw.rect("basement-shadow-glow", unit.x, unit.y, Time.time * -10);
-                }
-             });
-shadow.constructor = () => extend(UnitEntity, {});
-
 const carrier = extendContent(UnitType, "carrier", {});
 carrier.constructor = () => extend(UnitEntity, {
 });
