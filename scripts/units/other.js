b0abd87f0c35ec9a2f6c8d217bd566cf9eb3090a
Antlrr <81817551+MrAntlrr@users.noreply.github.com>
2021-09-17 22:01:15 +0800

    Update other.js

--- scripts/units/other.js
+++ scripts/units/other.js
@@ -6,3 +6,15 @@ const shadow = extend(UnitType, "shadow", {
                 }
              });
 shadow.constructor = () => extend(UnitEntity, {});
+
+const carrier = extendContent(UnitType, "carrier", {});
+carrier.constructor = () => extend(UnitEntity, {
+});
+
+const support-truck = extendContent(UnitType, "support-truck", {});
+support-truck.constructor = () => extend(UnitEntity, {
+});
+
+const oxypoly = extendContent(UnitType, "oxypoly", {});
+oxypoly.constructor = () => extend(UnitEntity, {
+});
